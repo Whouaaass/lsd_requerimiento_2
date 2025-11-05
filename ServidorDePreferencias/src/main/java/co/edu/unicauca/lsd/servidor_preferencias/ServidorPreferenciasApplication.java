7050ff2f8c6abb3d27e2ff9e89f1b2aff9d63f32
Whouaaass <fredyanaya@unicauca.edu.co>
2025-11-05 09:30:18 -0500

    Creacion servidor preferencias

---
+++ ServidorDePreferencias/src/main/java/co/edu/unicauca/lsd/servidor_preferencias/ServidorPreferenciasApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.lsd.servidor_preferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorPreferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorPreferenciasApplication.class, args);
+	}
+
+}
---
+++ ServidorDePreferencias/src/test/java/co/edu/unicauca/lsd/servidor_preferencias/ServidorPreferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.lsd.servidor_preferencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorPreferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
